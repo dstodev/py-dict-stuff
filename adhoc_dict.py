c5bc0412fe79c3eb99e46bd437437a6fbfe40ad6
Daniel Stotts <dstodev@gmail.com>
2022-08-21 22:57:46 -0700

    Better mixin behavior

--- adhoc_dict.py
+++ adhoc_dict.py
@@ -3,11 +3,8 @@ from typing import Any
 
 class AdhocDict(dict):
     def __getitem__(self, item: Any):
-        return self.get(item)
-
-    def get(self, *args: Any) -> Any:
-        value = super().get(*args)
+        value = self.get(item, None)
         if value is None:
             value = self.__class__()
-            self.__setitem__(args[0], value)
+            self.__setitem__(item, value)
         return value
--- attribute_dict.py
+++ attribute_dict.py
@@ -6,10 +6,10 @@ from convert_dict import ConvertDict
 
 class AttributeDict(AdhocDict, ConvertDict):
     def __getattr__(self, name: str) -> Any:
-        return super().get(name)
+        return self.__getitem__(name)
 
     def __setattr__(self, name: str, value: Any) -> None:
-        super().__setitem__(name, value)
+        self.__setitem__(name, value)
 
     def __delattr__(self, name: str) -> None:
-        super().__delitem__(name)
+        self.__delitem__(name)
